0cb5343e8761d366681e909c11f29fdeddbb44c6
kiaashour <52958018+kiaashour@users.noreply.github.com>
2019-08-21 21:32:13 +0100

    Update Text Cleaner.py

--- Text Cleaner.py
+++ Text Cleaner.py
@@ -10,7 +10,7 @@ for paper in papers:
         text_string = text.read()
         processed_text = text_processor.process(text_string, exclude_punct = True, make_phrases = True)
 
-        CleanTextPaper.write("Processed Text:\n\n")
+        CleanText.write("Processed Text:\n\n")
         count = 0
 
         for count, word in enumerate(processed_text[0]):
@@ -20,11 +20,11 @@ for paper in papers:
             if count % 100 == 0:
                 print("\n")
 
-        NormalisedMaterialsPaper.write("Materials:\n\n")         
+        NormalisedMaterials.write("Materials:\n\n")         
 
         for count, word in enumerate(processed_text[1]):
 
-            NormalisedMaterialsPaper.write("(%s, %s), " % (word[0], word[1]))
+            NormalisedMaterials.write("(%s, %s), " % (word[0], word[1]))
 
             if count % 100 == 0:
                 print("\n")        
