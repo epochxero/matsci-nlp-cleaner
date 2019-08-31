414f15fbd8f3a5577b98f4ed8fefce86dfe1bc76
Akul Vohra <vohra.akul@gmail.com>
2019-08-31 13:54:56 -0700

    Added a langdetect file for language detection. Encoding discrepancy causing errors.

--- TextProcessor.py
+++ TextProcessor.py
@@ -1,8 +1,9 @@
 from mat2vec.processing import MaterialsTextProcessor
 import pandas as pd
+from langdetect import detect
 
 #Extracts relevant material from .csv file
-initial_df = pd.read_csv('DataSet.csv', encoding="ISO-8859-1")
+initial_df = pd.read_csv('', encoding="ISO-8859-1")
 titles = (initial_df['Unnamed: 0.1'].head(100))
 abstracts, full_texts = (initial_df['Unnamed: 0.1.1'].head(100)), (initial_df['Title'].head(100))
 DOIs, pub_dates = (initial_df['Unnamed: 0'].head(100)), (initial_df['DOI'].head(100))
@@ -22,11 +23,10 @@ for paper in papers:
     processed_paper = text_processor.process(paper, exclude_punct = True, make_phrases = True)
     processed_texts += [" ".join(processed_paper[0])]
     norm_mats += [" ".join([material[1] for material in processed_paper[1]])]
-       
+
 
 #Output final data frame
 location = ""
 processed_data = {"DOIs": DOIs, "Publication Dates": pub_dates, "Titles": titles, "Processed Text": processed_texts, "Normalised Materials": norm_mats}
 final_df = pd.DataFrame(processed_data, columns = ["DOIs", "Publication Dates", "Titles", "Processed Text", "Normalised Materials"])
 final_df.to_csv(r"%s" % location, index = False)
-
