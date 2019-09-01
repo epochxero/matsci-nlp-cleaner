c417d594f53171ee3680c7df37d2be08d6daeb49
kiaashour <52958018+kiaashour@users.noreply.github.com>
2019-09-01 11:06:11 +0100

    References are split from text based on length, trivial entries also removed.

--- TextProcessor.py
+++ TextProcessor.py
@@ -2,17 +2,68 @@ from mat2vec.processing import MaterialsTextProcessor
 import pandas as pd
 from langdetect import detect
 
-#Extracts relevant material from .csv file
-initial_df = pd.read_csv('', encoding="ISO-8859-1")
-titles = (initial_df['Unnamed: 0.1'].head(100))
-abstracts, full_texts = (initial_df['Unnamed: 0.1.1'].head(100)), (initial_df['Title'].head(100))
-DOIs, pub_dates = (initial_df['Unnamed: 0'].head(100)), (initial_df['DOI'].head(100))
+#Preliminary cleaning of the data set; splits references based on length, removes empty/trivial rows.
+#Code for data frame columns: 0- DOI, 1- Title, 2- Abstract, 3- Date, 4- Text, 5- References
+#Cols: ['Unnamed: 0', 'Unnamed: 0.1', 'Unnamed: 0.1.1', 'DOI', 'Title', 'Abstract', 'Publication Date', 'Text', 'References']
+
+initial_df = pd.read_csv('DataSet.csv', encoding="utf8")
+initial_df = initial_df[~pd.isna(initial_df["Unnamed: 0.1.1"]) & ~pd.isna(initial_df["Title"])]
+
+#Iterates through the rows of the data frame; if references are absent, and the text/abstract is trivial, then remove the row.
+#If references are absent, and the text is non-trivial, scan for references in the text and split it. Reference sections are not updated.
+
+ref_synonyms = ["references:", "references", "reference:", "reference:", "bibliography", "bibliography:", "bibliographies:", "bibliographies",
+                "reference list:", "reference list", "citations:", "citations"]
+
+for row in initial_df.itertuples():
+
+    if pd.isna(row[6]) and (not pd.isna(row[5]) or not pd.isna(row[3])):
+                                                 
+      if len(row[5]) <= 100 and pd.isna(row[3]):
+
+          initial_df.drop(row[0], inplace = True)
+
+      else:
+                                                     
+          text = row[5].lower()
+          ref_found = False
+
+          for synonym in ref_synonyms:
+
+             if synonym in text and not ref_found:
+
+                 syn_indices = [match.start() for match in regex.finditer(synonym, text)]
+
+                 for syn_index in syn_indices:
+
+                     subtext = text[syn_index:]
+
+                     if len(subtext.split()) <= 300:
+
+                         ref_found = not ref_found
+                         new_text = text[:syn_index]
+                         initial_df.at[row[0], "Title"] = new_text
+                      
+                         break
+
+             elif ref_found:
+
+                 break
+                                     
+        
+                                                                                                    
+#Extracts relevant material from initial_df
+
+row_count = len(initial_df.index)
+titles = (initial_df['Unnamed: 0.1'].head(row_count))
+abstracts, full_texts, references = (initial_df['Unnamed: 0.1.1'].head(row_count), initial_df['Title'].head(row_count), initial_df["Abstract"].head(row_count))
+DOIs, pub_dates = (initial_df['Unnamed: 0'].head(row_count)), (initial_df['DOI'].head(row_count))
 
 
 #Adds full papers into an array; converts titles, DOIs, pub_dates to lists
-papers = [ abstracts[i] + " " + full_texts[i] for i in range(0,100)]
+abstracts, full_texts, references = list(abstracts), list(full_texts), list(references) 
 titles, DOIs, pub_dates = list(titles), list(DOIs), list(pub_dates)
-
+papers = [abstracts[i] + " " + full_texts[i] for i in range(0, row_count)]
 
 #Processes full papers and adds the processed text and normalised materials into a new .csv file
 text_processor = MaterialsTextProcessor()
@@ -23,7 +74,7 @@ for paper in papers:
     processed_paper = text_processor.process(paper, exclude_punct = True, make_phrases = True)
     processed_texts += [" ".join(processed_paper[0])]
     norm_mats += [" ".join([material[1] for material in processed_paper[1]])]
-
+       
 
 #Output final data frame
 location = ""
